5ce06895cf05adbd6f5a3d6ca7a3ae7fed8adbc9
Tomas Martinez <tomasagustinmartinez28@gmail.com>
2024-11-12 20:01:03 -0300

    home url

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { Route, Routes } from "react-router-dom"
 import MainContainer from "./components/MainContainer/MainContainer"
-
+import Home from "./components/Home/Home"
 function App() {
   return (
    
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/:chat_id" element={<MainContainer />} />
     </Routes>
 
